c2b4c514d730d5af12ecccd0d6e6b1e77f2ba7b4
Carlos Santana <carlos.santana33890@gmail.com>
2022-10-04 12:31:02 +0000

    insere(), remove()

--- Fila/Fila/Fila.cpp
+++ Fila/Fila/Fila.cpp
@@ -59,7 +59,7 @@ void menu()
 void inicializar()
 {
 
-	// se a lista já possuir elementos
+	// se a lista jÃ¡ possuir elementos
 	// libera a memoria ocupada
 	NO* aux = inicio;
 	while (aux != NULL) {
@@ -88,13 +88,24 @@ void insere()
 	cin >> novo->valor;
 	novo->prox = NULL;
 
-
+    if (inicio == NULL) {
+        inicio = novo;
+        fim = novo;
+    } else {
+        fim->prox = novo;
+        fim = novo;
+    }
 }
 
 void remove()
 {
-
-
-
+    if (inicio != NULL) {
+        cout << inicio->valor << " sera removido" << endl;
+        
+        NO* aux = inicio->prox;
+        free(inicio);
+        inicio = aux;
+    } else {
+        cout << "Fila vazia" << endl;
+    }
 }
-
